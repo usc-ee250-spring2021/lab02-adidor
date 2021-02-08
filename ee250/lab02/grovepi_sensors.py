fd2efd8a3d6f237e9d2729f2e029882a0bd2f34e
Adrien Dor <ador@usc.edu>
2021-02-07 21:27:46 -0800

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -30,10 +30,22 @@ is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will
 be true"""
 if __name__ == '__main__':
     PORT = 4    # D4
+    
+    # Connect the Grove Rotary Angle Sensor to analog port A0
+    # SIG,NC,VCC,GND
+    potentiometer = 0
+    grovepi.pinMode(potentiometer,"INPUT")
+
+    # Maximum distance threshold to be set with rotary angle sensor
+    maxThreshold = 50
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
+        
+        # Read angle  value from potentiometer
+        sensor_value = grovepi.analogRead(potentiometer)
+        setThreshold = int(sensor_value*maxThreshold/1024)
 
         print(grovepi.ultrasonicRead(PORT))
