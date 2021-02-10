ffdb3d61dd0562d8471fadd6348d2eee6914fa78
Adrien Dor <ador@usc.edu>
2021-02-10 15:36:31 -0800

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -5,8 +5,7 @@ Adrien Dor
 
 Insert Github repository link here.
 """
-git@github.com:usc-ee250-spring2021/lab02-adidor.git
-
+https://github.com/usc-ee250-spring2021/lab02-adidor.git
 """python3 interpreters in Ubuntu (and other linux distros) will look in a 
 default set of directories for modules when a program tries to `import` one. 
 Examples of some default directories are (but not limited to):
