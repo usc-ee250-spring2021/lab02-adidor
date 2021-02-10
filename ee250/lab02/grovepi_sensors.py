7b8de35f3e2ed81372cbc35c5632999544883a4b
Adrien Dor <ador@usc.edu>
2021-02-10 15:30:51 -0800

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,6 +1,7 @@
 """ EE 250L Lab 02: GrovePi Sensors
 
 List team members here.
+Adrien Dor
 
 Insert Github repository link here.
 """
@@ -24,12 +25,13 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+import grove_rgb_lcd
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
+    PORT = 3    # D3
     
     # Connect the Grove Rotary Angle Sensor to analog port A0
     # SIG,NC,VCC,GND
@@ -38,14 +40,31 @@ if __name__ == '__main__':
 
     # Maximum distance threshold to be set with rotary angle sensor
     maxThreshold = 50
+    
+    # Set RGB
+    setRGB(121, 19, 210)
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
         
+        
         # Read angle  value from potentiometer
         sensor_value = grovepi.analogRead(potentiometer)
         setThreshold = int(sensor_value*maxThreshold/1024)
-
+        
+        time.sleep(0.1)
+        print (setThreshold)
         print(grovepi.ultrasonicRead(PORT))
+        
+        # Compare if the threshold set by the rotary angle sensor is less than the
+        # distance measured by the ultrasonic sensor
+        if (grovepi.ultrasonicRead(PORT) < setThreshold):
+            lcdText = "{:>3d}".format(setThreshold) + "cm OBJ Pres  \n" + "{:>3d}".format(grovepi.ultrasonicRead(PORT)) + "cm"
+            setText_norefresh(lcdText)
+            setRGB(189, 83, 72)
+        else:
+            lcdText = "{:>3d}".format(setThreshold) + "cm           \n" + "{:>3d}".format(grovepi.ultrasonicRead(PORT)) + "cm"
+            setText_norefresh(lcdText)
+            setRGB(143, 189, 119)
