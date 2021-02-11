d2cf1bcaee92c427821c8889b22d245805b2c03f
Adrien Dor <ador@usc.edu>
2021-02-10 17:16:22 -0800

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -65,5 +65,5 @@ if __name__ == '__main__':
             setText_norefresh(txt)
  
         else:
-            txt = "{:>4d}".format(threshold) + "cm  \n" + "{:>4d}".format(grovepi.ultrasonicRead(PORT)) + "cm"
+            txt = "{:>4d}".format(threshold) + "cm        \n" + "{:>4d}".format(grovepi.ultrasonicRead(PORT)) + "cm"
             setText_norefresh(txt)
