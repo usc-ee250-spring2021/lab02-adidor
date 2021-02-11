e3642cfa744d1a30cdd49e0f1f189b7a568d74bb
Adrien Dor <ador@usc.edu>
2021-02-10 17:06:13 -0800

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -60,8 +60,8 @@ if __name__ == '__main__':
         # distance measured by the ultrasonic sensor
         if (grovepi.ultrasonicRead(PORT) < threshold):
             txt = "{:>4d}".format(threshold) + "cm OBJ Pres \n" + "{:>4d}".format(grovepi.ultrasonicRead(PORT)) + "cm"
-            setText(txt)
+            setText_norefresh(txt)
  
         else:
             txt = "{:>4d}".format(threshold) + "cm  \n" + "{:>4d}".format(grovepi.ultrasonicRead(PORT)) + "cm"
-            setText(txt)
+            setText_norefresh(txt)
